584a15a0424702b42f21f1600358a4e42e868ca0
KylerGreen <Kyleredmask@gmail.com>
2025-09-22 09:56:27 -0600

    Classwork for the day

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomnumber = new Random();
+        int magicnumber = randomnumber.Next(1, 101);
+        Console.WriteLine("Guess the number between 1 nad 100!");
+        bool guessed = false;
+
+        while (!guessed)
+        {   
+            Console.WriteLine("What is your guess?");
+            int numberguess = int.Parse(Console.ReadLine());
+            if (numberguess == magicnumber)
+            {
+                Console.WriteLine("You guessed the magic number");
+                guessed = true;
+            }
+            else if (numberguess < magicnumber)
+            {
+                Console.WriteLine("You guessed too low");
+            }
+            else
+            {
+                Console.WriteLine("You guessed too high");
+            }
+        }
     }
 }
