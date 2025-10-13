ed88ff65e6da501dd643e85c2491c5f7d3e50f31
KylerGreen <Kyleredmask@gmail.com>
2025-10-13 09:12:37 -0600

    changing to 9.0 rather than 8.0

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Card testCard = new Card();
+        // testCard._name = "Fireball";
+        // testCard._type = "Spell";
+        // testCard._cost = 4;
+        // testCard._description = "Deal 6 damage to all enemies.";
+        // testCard.Display();
     }
 }
